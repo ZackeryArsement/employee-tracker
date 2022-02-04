68cc3938f98a7041b1141a7a000d1906b9999dae
Zackery Arsement <zarsement@hotmail.com>
2022-02-04 17:39:20 -0600

    SQL queries work to view all three tables. Now need to add the ability to add new departments, roles, and employees.

--- server.js
+++ server.js
@@ -1,12 +1,78 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
-function showPrompt(){
-    inquirer.prompt(
+// Import query classes
+const Database = require('./lib/Database');
+const Departments = require('./lib/Departments');
+const Roles = require('./lib/Roles');
+const Employees = require('./lib/Employees');
+
+const databaseQuery = new Database();
+const roleQuery = new Roles();
+const employeeQuery = new Employees();
+
+
+async function showPrompt(){
+    const response = await inquirer.prompt(
     {
         type: 'list',
         name: 'selection',
         message: 'What would you like to do?',
         choices: ['View all Departments','Add Department','View all Roles','Add Role','View all Employees','Update an Employee Role','Add Employee']
-    }
-    )
-}
+    });
+
+    switch(response.selection){
+      case 'View all Departments':
+        callDepartment();
+        return;
+
+      case 'View all Roles':
+        callRole();
+        return;
+
+      case 'View all Employees':
+        callEmployee();
+        return;
+      };
+};
+
+async function callDepartment(){
+  try{
+    const data = await databaseQuery.showDepartment();
+    console.table(data);
+  }
+  catch(err){
+    console.log(err);
+  }
+  finally{
+    showPrompt()
+  }
+};
+
+async function callRole(){
+  try{
+    const data = await databaseQuery.showRole();
+    console.table(data);
+  }
+  catch(err){
+    console.log(err);
+  }
+  finally{
+    showPrompt()
+  }
+};
+
+async function callEmployee(){
+  try{
+    const data = await databaseQuery.showEmployee();
+    console.table(data);
+  }
+  catch(err){
+    console.log(err);
+  }
+  finally{
+    showPrompt()
+  }
+};
+
+showPrompt();
